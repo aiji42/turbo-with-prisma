17f5e8716c7d0fbed88fbb378242db6192c6fb05
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-02 00:51:46 +0900

    specify out path

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma-client";
 
 declare global {
   var prisma: PrismaClient | undefined;
--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -1,6 +1,6 @@
 import { prisma } from ".";
 
-import type { User } from "@prisma/client";
+import type { User } from "../generated/prisma-client";
 
 const DEFAULT_USERS = [
   // Add your own user to pre-populate the database with
