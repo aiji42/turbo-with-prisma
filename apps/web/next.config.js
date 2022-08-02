455d44bad6b1b0c9755b903214fe8c9f93d07cfa
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-02 09:54:00 +0900

    update next.config.js webpack

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,19 @@
 /**
  * @type {import('next').NextConfig}
  */
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /prisma-client\/index\.js$/,
+      loader: 'string-replace-loader',
+      options: {
+        search: 'config.dirname = dirname',
+        replace: `config.dirname = '${path.dirname(require.resolve('database/generated/prisma-client'))}'`,
+      }
+    })
+    return config
+  }
 };
